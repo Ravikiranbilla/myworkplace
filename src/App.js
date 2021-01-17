6336b1ca42db1a32e196177d2bb66976694462e7
Ravikiranbilla <ravikiranbillaa@gmail.com>
2021-01-17 21:48:22 +0530

    API-Post example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 
 import React from 'react';
+import axios from 'axios';
+import {Button, Modal} from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 import './App.css';
 class App extends React.Component {
@@ -11,51 +14,23 @@ class App extends React.Component {
          data1 : 0,
          data2: [],
          data3: ['teja', 'ravi', 'devika'],
-         data4 : [1, 2, 3, 4, 5]
+         data4 : [1, 2, 3, 4, 5],
+         show: false,
+         userId: '',
+         title: '',
+         body: ''
       }
    };
 
-   // componentDidMount() {
-   //    axios.get("https://jsonplaceholder.typicode.com/posts")
-   //    .then ((resp) => {
-   //       this.setState({data2: resp.data2});
-
-   //    }).catch ((error) => {
-
-   //    });
-   // }
-
-   // componentDidMount() {
-   //    axios
-   //    .get("https://jsonplaceholder.typicode.com/posts")
-   //    .then((response) => {
-   //      this.setState({data2: response.data2});
-   //    })
-   //    .catch((e) => 
-   //    {
-   //      console.error(e);
-   //    });
-   //  }
-
    componentDidMount() {
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              data2: result
-            });
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            this.setState({
-              data2: []
-            });
-          }
-        )
-    }
+      axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then ((resp) => {
+         this.setState({data2: resp.data});
+
+      }).catch ((error) => {
+         this.setState({data2: []});
+      });
+   }
     
    setNewName = () => {
       this.setState({data: "billa"})
@@ -71,8 +46,8 @@ class App extends React.Component {
                <ul>
                   {this.state.data2.map((item) => {
                      return(
-                        <React.Fragment>
-                         <li key={item.id}>
+                        <React.Fragment key={item.id}>
+                         <li>
                          {item.id}: {item.title}
                       </li>
                       <li>{item.body}</li>
@@ -114,16 +89,109 @@ class App extends React.Component {
             </>
          )
       }
+
+      handleClose = () => {
+         this.setState({show: false});
+         this.clearFormData();
+      }
+
+      handleShow=()=> {
+         this.setState({show: true});
+      }
+
+      handleChange = (e) => {
+         this.setState({[e.target.name]: e.target.value});
+      }
+
+      clearFormData = () => {
+         this.setState({
+            userId: '',
+            title: '',
+            body: ''
+         })
+      }
+
+      submitFormData = () => {
+         const userId = this.state.userId;
+         const title = this.state.title;
+         const body = this.state.body;
+         axios.post("https://jsonplaceholder.typicode.com/posts", {
+            "title": title,
+            "body": body,
+            "userId": userId
+         }).then((resp) => {
+            this.setState({show: false});
+            this.clearFormData();
+            const updateData = this.state.data2.concat({
+               "title": resp.data.title,
+               "body": resp.data.body,
+               "id": resp.data.id
+            });
+            this.setState({data2: updateData});
+         }).catch((err) => {
+
+         });
+      }
          render() {
       return (
          <div>
-            <button onClick = {this.setNewName}>Namechange</button>
+            {/* <button onClick = {this.setNewName}>Namechange</button>
             <div>{this.state.data}</div>
             <button onClick = {this.setNewNumber}>number increment  </button>
-            <div>{this.state.data1}</div>
-            {/* {this.getData()} */}
+            <div>{this.state.data1}</div> */}
+            {this.getData()}
             {/* {this.getData3()} */}
-            {this.getData4()}
+            {/* {this.getData4()} */}
+
+            <>
+      <Button variant="primary" onClick={() => this.handleShow()}>
+        Add New Post
+      </Button>
+
+      <Modal
+        show={this.state.show}
+        onHide={() => this.handleClose()}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+        <Form.Group controlId="formBasicEmail">
+            <Form.Label>User Id</Form.Label>
+            <Form.Control type="number" placeholder="Enter user id" 
+            name="userId"
+            value={this.state.userId}
+            onChange={(e) => this.handleChange(e)}/>
+         </Form.Group>
+         <Form.Group controlId="formBasicEmail">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Enter title" 
+            className="test"
+            name="title"
+            value={this.state.title}
+            onChange={(e) => this.handleChange(e)}/>
+         </Form.Group>
+         <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Body</Form.Label>
+    <Form.Control as="textarea" rows={3} 
+            name="body"
+            value={this.state.body}
+            onChange={(e) => this.handleChange(e)}/>
+  </Form.Group>
+         </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => this.handleClose()}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => this.submitFormData()}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+            
          </div>
       );
    }
